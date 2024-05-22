033144d6869b52cd768e26016e5d41184bb8f6b2
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-05-22 17:15:13 -0300

    point service

--- src/services/PointService.ts
+++ src/services/PointService.ts
@@ -2,33 +2,46 @@ import { prisma } from "@/libs/prisma";
 import { FindPointSchema } from "@/utils/schemas";
 import { FindPointType } from "@/utils/types";
 import { Prisma } from "@prisma/client";
+
+interface TeamInterface {
+  id: number
+}
 export class PointService {
   async findAll(data: FindPointType) {
     const games = await prisma.game.findMany({
       where: FindPointSchema.parse(data)
     })
-    
-    const gamesTeams =  games.map(game => (
+
+    const gamesTeams = games.map(game => (
       game.teams
     )).flat()
-    //let uniqueObjArray = [...new Map(objArray.map((item) => [item["id"], item])).values()];
-    // const teamsIds = [...new Set] gamesTeams.map((gameTeam:any) => gameTeam.id
-    // const teamsIds = [...new Set(gamesTeams.)]
-    // const teamsIds = []
-    
 
-    // console.log(gamesTeams, teamsIds)
+    console.log(gamesTeams)
 
-    return '
-    '
+    const teamsIds = [...new Map(gamesTeams.map((item: any) => [item.id, item.id])).values()];
 
-    return await prisma.team.findMany({
-      where:{
-        id:{
+    const teams = await prisma.team.findMany({
+      where: {
+        id: {
           in: teamsIds
         }
       }
     })
 
+    const pointsByGroup = gamesTeams.reduce<Record<string, {group:string, points: number}>>((acc, game:any) => {
+      const team = teams.find(team => team.id === game.id);
+
+      if (team) {
+        const group = team.group;
+        if (!acc[group]) {
+          acc[group] = { group, points: 0 };
+        }
+        acc[group].points += game.points;
+      }
+      return acc;
+    }, {});
+    
+    return Object.values(pointsByGroup).sort((a, b) =>  b.points - a.points)
+    
   }
 }
